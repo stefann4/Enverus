a9f3f24b94165ad89a5a34ed79017d84dacd74cd
stefann4 <stefann842@gmail.com>
2023-08-08 00:15:04 +0200

    Add project files.

---
+++ Enverus/Conversion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace Enverus
+{
+    public class Conversion
+    {
+        public void ConvertExcelToCsv()
+        {
+            var excelFilePath = "Book1.xlsx";
+            var csvFilePath = "data.csv";
+            string endYear = "2021";
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage(new FileInfo(excelFilePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+
+                using var csvWriter = new StreamWriter(csvFilePath);
+
+                for (int row = 1; row <= worksheet.Dimension.Rows; row++)
+                {
+                    var rowData = new List<string>();
+
+                    for (int col = 1; col <= worksheet.Dimension.Columns; col++)
+                    {
+
+                        if (endYear != Convert.ToString(worksheet.Cells[row, col].Value))
+                        {
+                            rowData.Add(worksheet.Cells[row, col].Value?.ToString() ?? "");
+                        }
+                        else
+                        {
+                            col = worksheet.Dimension.Columns;
+                            row = worksheet.Dimension.Rows;
+                            break;
+                        }
+                    }
+
+                    csvWriter.WriteLine(string.Join(",", rowData));
+                }
+            }
+        }
+    }
+}
---
+++ Enverus/Program.cs
@@ -0,0 +1,43 @@
+﻿using Enverus;
+using System.Net;
+using System.Net.Http;
+
+
+var conversion = new Conversion();
+
+var handler = new HttpClientHandler
+{
+    AllowAutoRedirect = true,
+    ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true,
+};
+
+using (HttpClient client = new(handler))
+{
+    client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+    var excelUrl = "https://bakerhughesrigcount.gcs-web.com/intl-rig-count?c=79687&p=irol-rigcountsintl/static-files/7240366e-61cc-4acb-89bf-86dc1a0dffe8";
+    try
+    {
+        client.BaseAddress = new Uri("https://bakerhughesrigcount.gcs-web.com/");
+
+        var response = await client.GetAsync(excelUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var file = await response.Content.ReadAsByteArrayAsync();
+
+            File.WriteAllBytes("Book1.xlsx", file);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+Console.WriteLine("Conversion starts...");
+
+conversion.ConvertExcelToCsv();
+
+Console.WriteLine("Conversion finished at " + DateTime.Now);
+Console.ReadKey();
